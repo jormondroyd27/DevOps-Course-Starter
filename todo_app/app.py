9c660cc72afd22a8973ae70d6ae7470bfa3f686d
James Ormondroyd <james.ormondroyd@kpmg.co.uk>
2021-11-10 14:51:19 +0000

    adadded get_items() argument as a parameter in render_template for index() function

--- todo_app/app.py
+++ todo_app/app.py
@@ -1,12 +1,14 @@
-from flask import Flask
-from flask.templating import render_template
-
+from flask import Flask, url_for, render_template, request
+from todo_app.data.session_items import get_items, add_item
 from todo_app.flask_config import Config
 
 app = Flask(__name__)
 app.config.from_object(Config())
 
 
-@app.route('/')
+@app.route('/', methods=["GET"])
 def index():
-    return render_template('index.html')
+    return render_template('index.html', get_items = get_items)
+
+if __name__ == "__main__":
+    app.run()
