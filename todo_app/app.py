88c9be0eae96ffd23f4fa3474a11ab8d98ba0e87
James Ormondroyd <james.ormondroyd@kpmg.co.uk>
2021-11-10 14:58:37 +0000

    added comments

--- todo_app/app.py
+++ todo_app/app.py
@@ -5,9 +5,10 @@ from todo_app.flask_config import Config
 app = Flask(__name__)
 app.config.from_object(Config())
 
-
+# routes the URL path
 @app.route('/', methods=["GET"])
 def index():
+    # renders index.html to the route '/' and passes the argument get_items() from session_items.py file
     return render_template('index.html', get_items = get_items)
 
 if __name__ == "__main__":
