169fd1f3c742da8145da6af4646c0fe244b2341f
James Ormondroyd <james.ormondroyd@kpmg.co.uk>
2022-03-31 17:16:34 +0100

    added changes for module 2 feedback with some testing features

--- todo_app/app.py
+++ todo_app/app.py
@@ -14,15 +14,9 @@ def create_app():
     app = Flask(__name__)
     app.config.from_object(Config())
 
-    SECRET_KEY = os.getenv('SECRET_KEY')
-    SECRET_TOKEN = os.getenv('SECRET_TOKEN')
-    SECRET_SECRET = os.getenv('SECRET_SECRET')
-    BOARD_ID = os.getenv('BOARD_ID')
-    CARD_ID = os.getenv('CARD_ID')
     TODO_LIST = os.getenv('todo_list')
     DOING_LIST = os.getenv('doing_list')
     DONE_LIST = os.getenv('done_list')
-    BOARD_ID = os.getenv('BOARD_ID')
 
     @app.route('/', methods=["GET"])
     def index():
--- todo_app/data/todo_item.py
+++ todo_app/data/todo_item.py
@@ -1,6 +1,3 @@
-from curses import A_DIM
-
-
 class TodoItem:
     def __init__(self, id, title) -> None:
         self.id = id
