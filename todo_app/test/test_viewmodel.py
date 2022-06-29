35a9f99ece8a4ee1b79b1a345f8280f0e873714c
James Ormondroyd <james.ormondroyd@kpmg.co.uk>
2022-06-29 12:22:07 +0100

    removing comments

--- todo_app/test/test_viewmodel.py
+++ todo_app/test/test_viewmodel.py
@@ -15,7 +15,3 @@ def test_done_items():
     item = TodoItem("123", "test item")
     viewmodel = ViewModel([], [], [item])
     assert viewmodel.done_lists == [item]
-
-# 1. A user can add a new item to the to do list - e2e
-# 2. The app can handle a response from Trello's API - integration
-# 3. The item sets a completed_at field to the current time when it is marked as 'Done' - unit
