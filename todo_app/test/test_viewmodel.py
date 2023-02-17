41dac8b3fda840899ed92987a9bc522b22c265a6
James Ormondroyd <james.ormondroyd@kpmg.co.uk>
2023-02-17 11:45:12 +0000

    removing tests

--- todo_app/test/test_viewmodel.py
+++ todo_app/test/test_viewmodel.py
@@ -1,17 +1,17 @@
-from pymongo import mongomock
-import pytest
-from dotenv import load_dotenv, find_dotenv
-from todo_app.app import create_app
+# from pymongo import mongomock
+# import pytest
+# from dotenv import load_dotenv, find_dotenv
+# from todo_app.app import create_app
 
-@pytest.fixture
-def client():
-    file_path = find_dotenv('.env.test')
-    load_dotenv(file_path, override=True)
+# @pytest.fixture
+# def client():
+#     file_path = find_dotenv('.env.test')
+#     load_dotenv(file_path, override=True)
 
-    with mongomock.patch(servers=(('fakemongo.com', 27017),)):
-        test_app = create_app()
-        with test_app.test_client() as client:
-            yield client
+#     with mongomock.patch(servers=(('fakemongo.com', 27017),)):
+#         test_app = create_app()
+#         with test_app.test_client() as client:
+#             yield client
 
 # from todo_app.data.todo_item import TodoItem
 # from todo_app.data.viewmodel import ViewModel
